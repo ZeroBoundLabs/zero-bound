7b1151aa8a649bbfca4e65434d26fbeb32b546e6
Usman Developer <usman.developer002@gmail.com>
2022-12-31 15:07:15 +0500

    Parse flight information

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html lang='en'>
+      <Head>
+        <title>Gmail Flight Reservations</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <body className='bg-gray-100'>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
