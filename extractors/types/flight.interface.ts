70be9eed9583dd18ffbded28dfb07eb305704b5c
James Farrell <james@toucan.earth>
2023-04-03 21:59:02 +0200

    feat: will use this as a better named template later

--- extractors/types/flight.interface.ts
+++ extractors/types/flight.interface.ts
@@ -11,3 +11,12 @@ export interface FlightDetails {
     arrivalTime: string
     airline: Airline
 }
+
+export interface FlightInfo {
+    departureAirport: string;
+    arrivalAirport: string;
+    departureDateTime: string;
+    arrivalDateTime: string;
+    flightDuration: string;
+    airline: Airline;
+  }
