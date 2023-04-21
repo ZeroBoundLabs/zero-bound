20c5a03167fea684aff0b9059e21572b23a6ec06
James Farrell <james@toucan.earth>
2023-04-21 11:32:15 +0200

    feat: get airport data from Hasura

---
+++ graphql/hasura/client.ts
@@ -0,0 +1,15 @@
+import { createClient } from 'urql';
+
+const { HASURA_SECRET, HASURA_URL } = process.env;
+
+export function getClient() {
+  return createClient({
+    url: HASURA_URL!,
+    fetchOptions: {
+      headers: {
+        'x-hasura-admin-secret': HASURA_SECRET!,
+      },
+    },
+  });
+}
+
